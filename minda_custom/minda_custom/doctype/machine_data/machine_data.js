2aa655d93e4101fe15da7266fd00ff4ec28c7724
Abdulla P I <hereabdulla@gmail.com>
2018-02-22 19:05:35 +0530

    added tag id&attendance_perfomace report

---
+++ minda_custom/minda_custom/doctype/machine_data/machine_data.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2018, Minda Sai Pvt LTd and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Machine Data', {
+	refresh: function(frm) {
+
+	}
+});
