416f53eff1406b2b18550a25d29f0cba45575f18
Abdulla P I <hereabdulla@gmail.com>
2018-03-04 20:17:13 +0530

    added wage monitor report doctype

---
+++ minda_custom/minda_custom/doctype/wage_monitor/wage_monitor.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2018, Minda Sai Pvt LTd and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Wage Monitor', {
+	refresh: function(frm) {
+
+	}
+});
